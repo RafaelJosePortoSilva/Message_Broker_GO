3f8ee8bc10f4b0ac0b5d55a3f928fba121d18530
RafaelJosePortoSilva <rafaelportot@proton.me>
2024-08-30 17:22:00 -0300

    Add Main

--- main.go
+++ main.go
@@ -75,3 +75,34 @@ func (b *Broker) Publish(topic string, payload interface{}) {
 		}
 	}
 }
+
+func main() {
+	broker := newBroker()
+
+	subscriber := broker.Subscribe("example_topic")
+	go func() {
+		for {
+			select {
+			case msg, ok := <-subscriber.Channel:
+				if !ok {
+					fmt.Println("Subscriber channel closed.")
+					return
+				}
+				fmt.Printf("Received: %v\n", msg)
+			case <-subscriber.Unsubscribe:
+				fmt.Println("Unsubscribed.")
+				return
+			}
+		}
+	}()
+
+	broker.Publish("example_topic", "Hello, World!")
+	broker.Publish("example_topic", "This is a test message.")
+
+	time.Sleep(2 * time.Second)
+	broker.Unsubscribe("example_topic", subscriber)
+
+	broker.Publish("example_topic", "This message won't be received.")
+
+	time.Sleep(time.Second)
+}
