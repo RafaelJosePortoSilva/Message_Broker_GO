ee4b4442cf904d69583ccd75548c2dca303c622e
RafaelJosePortoSilva <rafaelportot@proton.me>
2024-08-30 15:28:46 -0300

    Add Subscribe

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	_ "fmt"
+	"sync"
+	_ "time"
+)
+
+type Message struct {
+	Topic   string
+	Payload interface{}
+}
+
+type Subscriber struct {
+	Channel     chan interface{}
+	Unsubscribe chan bool
+}
+
+type Broker struct {
+	subscribers map[string][]*Subscriber
+	mutex       sync.Mutex
+}
+
+func newBroker() *Broker {
+	return &Broker{
+		subscribers: make(map[string][]*Subscriber),
+	}
+}
+
+func (b *Broker) Subscribe(topic string) *Subscriber {
+
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
+	subscriber := &Subscriber{
+		Channel:     make(chan interface{}, 1),
+		Unsubscribe: make(chan bool),
+	}
+	b.subscribers[topic] = append(b.subscribers[topic], subscriber)
+
+	return subscriber
+}
